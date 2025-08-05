316df7709f3400943407ed17e0b31b45b5df40f0
Hyeonju03 <wn1234wn@naver.com>
2025-08-05 12:23:38 +0900

    프로젝트 초기화

--- hantalk/src/main/java/com/example/hantalk/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hantalk.repository;
-
-import com.example.hantalk.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
--- hantalk/src/main/java/com/example/hantalk/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.hantalk.repository;
-
-import com.example.hantalk.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends JpaRepository<Item, Integer> {
-}
